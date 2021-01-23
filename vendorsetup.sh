2dd625acc40ff4cebdf3a9713da85ce84a27d068
LichtEstarossx <44682525+LichtEstarossx@users.noreply.github.com>
2021-01-22 18:36:02 -0600

    pokerp:removed unused settings

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -34,13 +34,9 @@ if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
 fi
 
 if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
-	export FOX_VERSION="11"
-	export OF_MAINTAINER="LichtEstarossx"
-        export FOX_BUILD_TYPE="Unofficial"
         export PLATFORM_VERSION="16.1.0"
    	export PLATFORM_SECURITY_PATCH="2099-12-31"
    	export TW_DEFAULT_LANGUAGE="en"
-        export OF_FLASHLIGHT_ENABLE="0"
 	export OF_SCREEN_H="2280"
         export OF_STATUS_H="83"
         export OF_STATUS_INDENT_LEFT="52"
@@ -50,17 +46,18 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 	export OF_AB_DEVICE="1"
 	export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER="1"
 	#export OF_FORCE_MAGISKBOOT_BOOT_PATCH_MIUI=1; # if you disable this, then enable the next line
-
-	export FOX_USE_BASH_SHELL="0"
-	export FOX_ASH_IS_BASH="0"
+	
 	export FOX_USE_NANO_EDITOR="1"
 	export FOX_USE_TAR_BINARY="1"
 	export FOX_USE_ZIP_BINARY="1"
-	export FOX_REPLACE_BUSYBOX_PS="0"
 	export OF_USE_NEW_MAGISKBOOT="1"
 	export OF_SKIP_MULTIUSER_FOLDERS_BACKUP="1"
 	export OF_VANILLA_BUILD="1"
 	export OF_DISABLE_UPDATEZIP="1"
+	
+	#Build type and mantainer info
+	export OF_MAINTAINER="LichtEstarossx"
+        export FOX_BUILD_TYPE="Unofficial"
 
 	# OTA for custom ROMs
         export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES="1"
@@ -68,7 +65,6 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 
         # -- add settings for R11 --
         export FOX_R11="1"
-        export FOX_ADVANCED_SECURITY="0"
         export OF_USE_TWRP_SAR_DETECT="1"
 	export OF_USE_TWRP_SAR_DETECT="1"
         export OF_DISABLE_MIUI_OTA_BY_DEFAULT="1"
